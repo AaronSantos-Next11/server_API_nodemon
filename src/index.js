add6989e8c6b1d7b95ead4553d7e9b161a6e37db
Aaron Santos <asantosabsalon@gmail.com>
2025-03-27 11:17:30 -0500

    add: Uso de variables de entorno para la base de datos

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const app = require('./app.js'); // const app = import('./app');
 app.listen(app.get('port'), () => {
 
    console.log('Servidor escuchando en el puerto', app.get('port'));
+   console.log('http://localhost:' + app.get('port') + '/api/clientes/')
+   console.log('http://localhost:' + app.get('port') + '/api/user/')
 
 })
-
-
