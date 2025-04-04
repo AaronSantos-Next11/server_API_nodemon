6e6a8f9678df68bd209a3729c5afd9758fe0b7c8
Aaron Santos <asantosabsalon@gmail.com>
2025-04-03 19:55:47 -0500

    add: objeto de configuracion para acceder a la base de datos

--- src/config.js
+++ src/config.js
@@ -1,8 +1,13 @@
 require('dotenv').config();
 
 module.exports = {
-
    app:{
-      port: process.env.PORT || 5000,
+      port: process.env.PORT || 4000
+   },
+   mysql: {
+      host:process.env.MYSQL_HOST,
+      user: process.env.MYSQL_USER,
+      password: process.env.MYSQL_PASSWORD,
+      database: process.env.MYSQL_BD
    }
 }
